25c2e1761d109df2dc2f66b0fe0088c1971162dd
Qiang Fu <tactiturn@hotmail.com>
2024-03-23 02:15:49 +0800

    Add model of transactions. Add preview data

---
+++ expense_tracker/TransactionModel.swift
@@ -0,0 +1,29 @@
+//
+//  Transaction.swift
+//  expense_tracker
+//
+//  Created by Michael on 3/23/24.
+//
+
+import Foundation
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date:String
+    let institution: String
+    let account: String
+    var merchant: String // Var can be changed
+    let amount: Double
+    let type: TransactionType.RawValue  //MARK String version
+    var categoryId: Int
+    var category: String
+    let isPending: Bool
+    var IsTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+}
+
+enum TransactionType:String {
+    case debit = "debit"
+    case credit = "credit"
+}
