243cefa01fb83e9d7f81581dd2ff47027cb04607
Qiang Fu <tactiturn@hotmail.com>
2024-03-23 00:35:31 +0800

    Added Navigation view on the content view. Set the Background and Navigation title display with notification Added Scrollview with Stack. Add Overview on the stack in scrollviews

---
+++ expense_tracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  expense_tracker
+//
+//  Created by Michael on 3/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
